3ab07cfb81fb7da7c1c326629d841e510aa18f6c
llcy531 <2212731@mail.nankai.edu.cn>
2025-06-27 23:13:03 +0800

    modify B2

--- extensions.py
+++ extensions.py
@@ -5,3 +5,10 @@ from flask_cors import CORS
 db = SQLAlchemy()
 jwt = JWTManager()
 cors = CORS()
+
+## 修改 B2
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/ocean_farm'
+# 1. 密码留空写法（注意冒号后面没有密码）
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost/ocean_farm'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = True
